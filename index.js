8f3777d21e324e63714b2544d005f7e24bfb9525
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-05-03 03:42:12 +0600

    added career bio data

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.get('/allData', (req, res)=>{
     res.send(allData)
 })
 
+app.get('/allData/:id', (req, res)=>{
+    const id =req.params.id;
+    console.log(id);
+    const singleData = allData.find(n=>n.id == id)
+    res.send(singleData)
+    
+})
+
 app.listen(port, ()=>{
     console.log(`Assignment server API is running on port: ${port}`)
 })
